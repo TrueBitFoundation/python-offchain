bea5660d2000a117c25baadc8e2a4e62c262c0f9
bloodstalker <thabogre@gmail.com>
2017-07-12 17:21:29 +0430

    wip- assembling the type section

--- argparser.py
+++ argparser.py
@@ -18,120 +18,138 @@ class Colors:
 
 
 class WASM_OP_Code:
-    type_ops = [{'i32', '0x7f'}, {'i64', '0x7e'}, {'f32', '0x7d'},
-                {'f64', '0x7c'}, {'anyfunc', '0x7b'}, {'func', '0x60'},
-                {'empty_block_type', '0x40'}]
+    type_ops = [('i32', '7f'), ('i64', '7e'), ('f32', '7d'),
+                ('f64', '7c'), ('anyfunc', '7b'), ('func', '60'),
+                ('empty_block_type', '40')]
+    type_ops_dict = dict(type_ops)
 
-    control_flow_ops = [{'unreachable', '0x00'}, {'nop', '0x01'},
-                        {'block', '0x02'}, {'loop', '0x03'},
-                        {'if', '0x04'}, {'else', '0x05'},
-                        {'end', '0x0b'}, {'br', '0x0c'},
-                        {'br_if', '0x0d'}, {'br_table', '0x0e'},
-                        {'return', '0x0f'}]
+    control_flow_ops = [{'unreachable', '00'}, {'nop', '01'},
+                        {'block', '02'}, {'loop', '03'},
+                        {'if', '04'}, {'else', '05'},
+                        {'end', '0b'}, {'br', '0c'},
+                        {'br_if', '0d'}, {'br_table', '0e'},
+                        {'return', '0f'}]
+    control_flow_ops_dict = dict(control_flow_ops)
 
-    call_ops = [{'call', '0x10'}, {'call_indirect', '0x11'}]
+    call_ops = [{'call', '10'}, {'call_indirect', '11'}]
+    call_ops_dict = dict(call_ops)
 
-    param_ops = [{'drop', '0x1a'}, {'select', '0x1b'}]
+    param_ops = [{'drop', '1a'}, {'select', '1b'}]
+    param_ops_dict = dict(param_ops)
 
-    var_access = [{'get_local', '0x20'}, {'set_local', '0x21'},
-                    {'tee_local', '0x22'}, {'get_global', '0x23'},
-                    {'set_global', '0x24'}]
+    var_access = [{'get_local', '20'}, {'set_local', '21'},
+                    {'tee_local', '22'}, {'get_global', '23'},
+                    {'set_global', '24'}]
+    var_access_dict = dict(var_access)
 
-    mem_ops = [{'i32.load', '0x28'}, {'i64.load', '0x29'},
-                {'f32.load', '0x2a'}, {'f64.load', '0x2b'},
-                {'i32.load8_s', '0x2c'}, {'i32.load8_u', '0x2d'},
-                {'i32.load16_s', '0x2e'},  {'i32.load16_u', '0x2f'},
-                {'i64.load8_s', '0x30'}, {'i64.load8_u', '0x31'},
-                {'i64.load16_s', '0x32'}, {'i64.load16_u', '0x33'},
-                {'i64.load32_s', '0x34'}, {'i64.load32_u', '0x35'},
-                {'i32.store', '0x36'}, {'i64.store', '0x37'},
-                {'f32.store', '0x38'}, {'f64.store', '0x39'},
-                {'i32.store8', '0x3a'}, {'i32.store16', '0x3b'},
-                {'i64.store8', '0x3c'}, {'i64.store16', '0x3d'},
-                {'i64.store32', '0x3e'}, {'current_memory', '0x3f'},
-                {'grow_memory', '0x40'}]
+    mem_ops = [{'i32.load', '28'}, {'i64.load', '29'},
+                {'f32.load', '2a'}, {'f64.load', '2b'},
+                {'i32.load8_s', '2c'}, {'i32.load8_u', '2d'},
+                {'i32.load16_s', '2e'},  {'i32.load16_u', '2f'},
+                {'i64.load8_s', '30'}, {'i64.load8_u', '31'},
+                {'i64.load16_s', '32'}, {'i64.load16_u', '33'},
+                {'i64.load32_s', '34'}, {'i64.load32_u', '35'},
+                {'i32.store', '36'}, {'i64.store', '37'},
+                {'f32.store', '38'}, {'f64.store', '39'},
+                {'i32.store8', '3a'}, {'i32.store16', '3b'},
+                {'i64.store8', '3c'}, {'i64.store16', '3d'},
+                {'i64.store32', '3e'}, {'current_memory', '3f'},
+                {'grow_memory', '40'}]
+    mem_ops_dict = dict(mem_ops)
 
-    consts = [{'i32.const', '0x41'}, {'i64.const', '0x42'},
-              {'f32.const', '0x43'}, {'f64', '0x44'}]
+    consts = [{'i32.const', '41'}, {'i64.const', '42'},
+              {'f32.const', '43'}, {'f64', '44'}]
+    consts_dict = dict(consts)
 
-    comp_ops = [{'i32.eqz', '0x45'}, {'i32.eq', '0x46'}, {'i32.ne', '0x47'},
-                {'i32.lt_s', '0x48'}, {'i32.lt_u', '0x49'},
-                {'i32.gt_s', '0x4a'}, {'i32.gt_u', '0x4b'},
-                {'i32.le_s', '0x4c'}, {'i32.le_u', '0x4d'},
-                {'i32.ge_s', '0x4e'}, {'i32.ge_u', '0x4f'},
-                {'i64.eqz', '0x50'}, {'i64.eq', '0x51'},
-                {'i64.ne', '0x52'}, {'i64.lt_s', '0x53'},
-                {'i64.lt_u', '0x54'}, {'i64.gt_s', '0x55'},
-                {'i64.gt_u', '0x56'}, {'i64.le_s', '0x57'},
-                {'i64.le_u', '0x58'}, {'i64.ge_s', '0x59'},
-                {'i64.ge_u', '0x5a'}, {'f32.eq', '0x5b'},
-                {'f32.ne', '0x5c'}, {'f32.lt', '0x5d'},
-                {'f32.gt', '0x5e'}, {'f32.le', '0x5f'},
-                {'f32.ge', '0x60'}, {'f64.eq', '0x61'},
-                {'f64.ne', '0x62'}, {'f64.lt', '0x63'},
-                {'f64.gt', '0x64'}, {'f64.le', '0x65'},
-                {'f64.ge', '0x66'}]
+    comp_ops = [{'i32.eqz', '45'}, {'i32.eq', '46'}, {'i32.ne', '47'},
+                {'i32.lt_s', '48'}, {'i32.lt_u', '49'},
+                {'i32.gt_s', '4a'}, {'i32.gt_u', '4b'},
+                {'i32.le_s', '4c'}, {'i32.le_u', '4d'},
+                {'i32.ge_s', '4e'}, {'i32.ge_u', '4f'},
+                {'i64.eqz', '50'}, {'i64.eq', '51'},
+                {'i64.ne', '52'}, {'i64.lt_s', '53'},
+                {'i64.lt_u', '54'}, {'i64.gt_s', '55'},
+                {'i64.gt_u', '56'}, {'i64.le_s', '57'},
+                {'i64.le_u', '58'}, {'i64.ge_s', '59'},
+                {'i64.ge_u', '5a'}, {'f32.eq', '5b'},
+                {'f32.ne', '5c'}, {'f32.lt', '5d'},
+                {'f32.gt', '5e'}, {'f32.le', '5f'},
+                {'f32.ge', '60'}, {'f64.eq', '61'},
+                {'f64.ne', '62'}, {'f64.lt', '63'},
+                {'f64.gt', '64'}, {'f64.le', '65'},
+                {'f64.ge', '66'}]
+    comp_ops_dict = dict(comp_ops)
 
-    num_ops = [{'i32.clz', '0x67'}, {'i32.ctz', '0x68'},
-               {'i32.popcnt', '0x69'}, {'i32.add', '0x6a'},
-               {'i32.sub', '0x6b'}, {'i32.mul', '0x6c'},
-               {'i32.div_s', '0x6d'}, {'i32.div_u', '0x6e'},
-               {'i32.rem_s', '0x6e'}, {'i32.rem_u', '0x70'},
-               {'i32.and', '0x71'}, {'i32.or', '0x72'},
-               {'i32.xor', '0x73'}, {'i32.shl', '0x74'},
-               {'i32.shr_s', '0x75'}, {'i32.shr_u', '0x76'},
-               {'i32.rotl', '0x77'}, {'i32.rotr', '0x78'},
-               {'i64.clz', '0x79'}, {'i64.ctz', '0x7a'},
-               {'i64.popcnt', '0x7b'}, {'i64.add', '0x7c'},
-               {'i64.sub', '0x7d'}, {'i64.mul', '0x7e'},
-               {'i64.div_s', '0x7f'}, {'i64.div_u', '0x80'},
-               {'i64.rem_s', '0x81'}, {'i64.rem_u', '0x82'},
-               {'i64.and', '0x83'}, {'i64.or', '0x84'},
-               {'i64.xor', '0x85'}, {'i64.shl', '0x86'},
-               {'i64.shr_s', '0x87'}, {'i64.shr_u', '0x88'},
-               {'i64.rotl', '0x89'}, {'i63.rotr', '0x8a'},
-               {'f32.abs', '0x8b'}, {'f32.neg', '0x8c'},
-               {'f32.ceil', '0x8d'},  {'f32.floor', '0x8e'},
-               {'f32.trunc', '0x8f'}, {'f32.nearest', '0x90'},
-               {'f32.sqrt', '0x91'}, {'f32.add', '0x92'},
-               {'f32.sub', '0x93'}, {'f32.mul', '0x94'},
-               {'f32.div', '0x95'}, {'f32.min', '0x96'},
-               {'f32.max', '0x97'}, {'f32.copysign', '0x98'},
-               {'f64.abs', '0x99'}, {'f64.neg', '0x9a'},
-               {'f64.ceil', '0x9b'}, {'f64.floor', '0x9c'},
-               {'f64.trunc', '0x9d'}, {'f64.nearest', '0x9e'},
-               {'f64.sqrt', '0x9f'}, {'f64.add', '0xa0'},
-               {'f64.sub', '0xa1'}, {'f64.mul', '0xa2'},
-               {'f64.div', '0xa3'}, {'f64.min', '0xa4'},
-               {'f64.max', '0xa5'}, {'f64.copysign', '0xa6'}]
+    num_ops = [{'i32.clz', '67'}, {'i32.ctz', '68'},
+               {'i32.popcnt', '69'}, {'i32.add', '6a'},
+               {'i32.sub', '6b'}, {'i32.mul', '6c'},
+               {'i32.div_s', '6d'}, {'i32.div_u', '6e'},
+               {'i32.rem_s', '6e'}, {'i32.rem_u', '70'},
+               {'i32.and', '71'}, {'i32.or', '72'},
+               {'i32.xor', '73'}, {'i32.shl', '74'},
+               {'i32.shr_s', '75'}, {'i32.shr_u', '76'},
+               {'i32.rotl', '77'}, {'i32.rotr', '78'},
+               {'i64.clz', '79'}, {'i64.ctz', '7a'},
+               {'i64.popcnt', '7b'}, {'i64.add', '7c'},
+               {'i64.sub', '7d'}, {'i64.mul', '7e'},
+               {'i64.div_s', '7f'}, {'i64.div_u', '80'},
+               {'i64.rem_s', '81'}, {'i64.rem_u', '82'},
+               {'i64.and', '83'}, {'i64.or', '84'},
+               {'i64.xor', '85'}, {'i64.shl', '86'},
+               {'i64.shr_s', '87'}, {'i64.shr_u', '88'},
+               {'i64.rotl', '89'}, {'i63.rotr', '8a'},
+               {'f32.abs', '8b'}, {'f32.neg', '8c'},
+               {'f32.ceil', '8d'},  {'f32.floor', '8e'},
+               {'f32.trunc', '8f'}, {'f32.nearest', '90'},
+               {'f32.sqrt', '91'}, {'f32.add', '92'},
+               {'f32.sub', '93'}, {'f32.mul', '94'},
+               {'f32.div', '95'}, {'f32.min', '96'},
+               {'f32.max', '97'}, {'f32.copysign', '98'},
+               {'f64.abs', '99'}, {'f64.neg', '9a'},
+               {'f64.ceil', '9b'}, {'f64.floor', '9c'},
+               {'f64.trunc', '9d'}, {'f64.nearest', '9e'},
+               {'f64.sqrt', '9f'}, {'f64.add', 'a0'},
+               {'f64.sub', 'a1'}, {'f64.mul', 'a2'},
+               {'f64.div', 'a3'}, {'f64.min', 'a4'},
+               {'f64.max', 'a5'}, {'f64.copysign', 'a6'}]
+    num_ops_dict = dict(num_ops)
 
-    conversion = [{'i32.wrap/i64', '0xa7'},
-                    {'i32.wrap/i64', '0xa7'},
-                    {'i32.trunc_s/f32', '0xa8'},
-                    {'i32.trunc_u/f32', '0xa9'},
-                    {'i32.trunc_s/f64', '0xaa'},
-                    {'i32.trunc_u/f64', '0xab'},
-                    {'i64.extend_s/i32', '0xac'},
-                    {'i64.extend_u/i32', '0xad'},
-                    {'i64.trunc_s/f32', '0xae'},
-                    {'i64.trunc_u/f32', '0xaf'},
-                    {'i64.trunc_s/f64', '0xb0'},
-                    {'i64.trunc_u/f64', '0xb1'},
-                    {'f32.convert_s/i32', '0xb2'},
-                    {'f32.convert_u/i32', '0xb3'},
-                    {'f32.convert_s/i64', '0xb4'},
-                    {'f32.convert_u/i64', '0xb5'},
-                    {'f32.demote/f64', '0xb6'},
-                    {'f64.convert_s/i32', '0xb7'},
-                    {'f64.convert_u/i32', '0xb8'},
-                    {'f64.convert_s/i64', '0xb9'},
-                    {'f64.convert_u/i64', '0xba'},
-                    {'f64.promote/f32', '0xbb'}]
+    conversion = [{'i32.wrap/i64', 'a7'},
+                    {'i32.trunc_s/f32', 'a8'},
+                    {'i32.trunc_u/f32', 'a9'},
+                    {'i32.trunc_s/f64', 'aa'},
+                    {'i32.trunc_u/f64', 'ab'},
+                    {'i64.extend_s/i32', 'ac'},
+                    {'i64.extend_u/i32', 'ad'},
+                    {'i64.trunc_s/f32', 'ae'},
+                    {'i64.trunc_u/f32', 'af'},
+                    {'i64.trunc_s/f64', 'b0'},
+                    {'i64.trunc_u/f64', 'b1'},
+                    {'f32.convert_s/i32', 'b2'},
+                    {'f32.convert_u/i32', 'b3'},
+                    {'f32.convert_s/i64', 'b4'},
+                    {'f32.convert_u/i64', 'b5'},
+                    {'f32.demote/f64', 'b6'},
+                    {'f64.convert_s/i32', 'b7'},
+                    {'f64.convert_u/i32', 'b8'},
+                    {'f64.convert_s/i64', 'b9'},
+                    {'f64.convert_u/i64', 'ba'},
+                    {'f64.promote/f32', 'bb'}]
+    conversion_dict = dict(conversion)
 
-    reinterpretations = [{'i32.reinterpret/f32', '0xbc'},
-                         {'i64.reinterpret/f64', '0xbd'},
-                         {'f32.reinterpret/i32', '0xbe'},
-                         {'f64.reinterpret/i64', '0xbf'}]
+    reinterpretations = [{'i32.reinterpret/f32', 'bc'},
+                         {'i64.reinterpret/f64', 'bd'},
+                         {'f32.reinterpret/i32', 'be'},
+                         {'f64.reinterpret/i64', 'bf'}]
+    reinterpretations_dict = dict(reinterpretations)
+
+    section_code = [{'type', '01'}, {'import', '02'},
+                    {'function', '03'}, {'table', '04'},
+                    {'memory', '05'}, {'global', '06'},
+                    {'export', '07'}, {'start', '08'},
+                    {'element', '09'}, {'code', '0a'},
+                    {'data', '0b'}]
+    section_code_dict = dict(section_code)
 
 
 class CLIArgParser(object):
@@ -389,7 +407,7 @@ class FuncBodyParser(object):
                 print ('-----------------------------')
                 elem.split
 
-    def ParseBodyV3(self):
+    def ParseBodyV3(self, Print):
         stack = []
         expr = []
         full = []
@@ -399,9 +417,11 @@ class FuncBodyParser(object):
             for stackval in re.finditer(sexpr_greedy,
                                         self.wast_obj_func[funcbody]):
                 for k, v in stackval.groupdict().items():
-                    if v is not None:
-                        print(k, v)
+                    if Print:
+                        if v is not None:
+                            print(k, v)
 
+                    if v is not None:
                         if k == 'rparen':
                             flag = True
                             expr.append(v)
@@ -422,10 +442,14 @@ class FuncBodyParser(object):
 
 class WASM_CodeEmitter(object):
     Obj_file = []
+    Obj_Header = []
     little_endian = True
 
-    def __init__(self, expr_stack):
-        self.expr_stack = expr_stack
+    def __init__(self, stack):
+        self.stack = stack
+
+    def SetNewStack(self, new_stack):
+        self.stack = new_stack
 
     def Obj_Header_32(self):
         magic_number = '0061736d'
@@ -433,6 +457,60 @@ class WASM_CodeEmitter(object):
 
         self.Obj_file.append(magic_number)
         self.Obj_file.append(version)
+
+    def EmitTypeHeader(self):
+        val_cnt = 0
+        byte_cnt = 0
+        word = str()
+        param_sentence = str()
+        result_sentence = str()
+        tmp_obj = []
+
+        for stacks in self.stack:
+            for stack_value in stacks:
+                if stack_value in WASM_OP_Code.type_ops_dict:
+                    if stack_value == 'func':
+                        tmp_obj.append(WASM_OP_Code.type_ops_dict[stack_value])
+                        if param_sentence != "":
+                            tmp_obj.append(param_sentence)
+                        else:
+                            tmp_obj.append("00")
+                            byte_cnt += 1
+
+                        if result_sentence != "":
+                            tmp_obj.append(result_sentence)
+                        else:
+                            tmp_obj.append("00")
+                            byte_cnt += 1
+
+                        param_sentence = ""
+                        result_sentence = ""
+                        val_cnt = 0
+                    else:
+                        word += WASM_OP_Code.type_ops_dict[stack_value]
+                        val_cnt += 1
+                    byte_cnt += 1
+                elif stack_value == 'param':
+                    param_sentence += str(bytes([val_cnt])) + word
+                    byte_cnt += 1
+                    val_cnt = 0
+                    word = ""
+                elif stack_value == 'result':
+                    result_sentence += str(bytes([val_cnt])) + word
+                    byte_cnt += 1
+                    val_cnt = 0
+                    word = ""
+
+        tmp_obj.insert(0, '8080')
+        tmp_obj.insert(0, format(byte_cnt, 'x'))
+        tmp_obj.insert(0, WASM_OP_Code.section_code_dict['type'])
+        self.Obj_Header = tmp_obj
+
+    def PrintTypeHeaderObj(self):
+        # print(self.Obj_Header)
+
+        for byte in self.Obj_Header:
+            print(byte)
 
     def Dump_Obj_STDOUT(self):
         for bytecode in self.Obj_file:
@@ -458,11 +536,20 @@ class ParserV1(object):
         wasmtobj.FuncParser()
         if __DBG__:
             wasmtobj.FuncParserTest()
+
         funcbodyparser = FuncBodyParser(wasmtobj.getFuncBodies())
-        expr_stack = funcbodyparser.ParseBodyV3()
+        headerparser = FuncBodyParser(wasmtobj.getTypeHeader())
+
+        expr_stack = funcbodyparser.ParseBodyV3(False)
+        header_stack = headerparser.ParseBodyV3(True)
+
         wasm_codeemitter = WASM_CodeEmitter(expr_stack)
         wasm_codeemitter.Obj_Header_32()
         wasm_codeemitter.Dump_Obj_STDOUT()
+
+        wasm_codeemitter.SetNewStack(header_stack)
+        wasm_codeemitter.EmitTypeHeader()
+        wasm_codeemitter.PrintTypeHeaderObj()
 
 
 def main():
