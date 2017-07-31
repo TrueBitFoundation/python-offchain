4ef74f39b1c39975ef66654f9004c6e662131022
bloodstalker <thabogre@gmail.com>
2017-07-31 16:17:33 +0430

    added a contributions file and the truebit high-level source code checker

---
+++ Contributions.md
@@ -0,0 +1,15 @@
+## Contributations
+
+### Structure
+This section introduces the various files and directories inside the repository:<br/>
+* `argparser.py` holds the main body of the interpreter.<br/>
+* `Contributations.md` is the current document you're viewing.<br/>
+* `c-samples` holds some simple C code and its equivalent WASM text format<br/>
+* `getobj.sh` was used to the hex byte-code from the object file. deprecated.<br/>
+* `injected.wasm` is an object file used for testing.<br/>
+* `injected.wast` is the WASM text representation of the same test file.<br/>
+* `QuestionnTasks.md` holds the current tasks that need to be done and the current questions/challenges that we are facing.<br/>
+* `README.md` is the readme. You should first read that.<br/>
+* `TBInit.py` is the file that holds the containers for the Trueit Interpreter's internal state.<br/>
+* `test_LEB128.py` holds the tests for the LEB128 encoders.<br/>
+* `TBC` the directory holds the checker that enforces the conditions on the high-level source code that is going to run by the interpreter.<br/>
---
+++ TBC/TBC.cpp
@@ -0,0 +1,86 @@
+
+/****************************************************Truebit Checker****************************************************/
+//-*-c++-*-
+/*standard headers*/
+/*LLVM headers*/
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Lex/Lexer.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "llvm/IR/Function.h"
+/**********************************************************************************************************************/
+/*used namespaces*/
+using namespace llvm;
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace clang::driver;
+using namespace clang::tooling;
+/**********************************************************************************************************************/
+/*global vars*/
+static llvm::cl::OptionCategory TBCCategory("TBC");
+/**********************************************************************************************************************/
+/*The Matchers go here*/
+/**********************************************************************************************************************/
+/**********************************************************************************************************************/
+class TBCASTConsumer : public ASTConsumer {
+
+public:
+  TBCASTConsumer(Rewriter &R){
+  }
+
+  void HandleTranslationUnit(ASTContext &Context) override {
+    Matcher.matchAST(Context);
+  }
+
+private:
+  MatchFinder Matcher;
+};
+/**********************************************************************************************************************/
+/**
+ * @brief a blank diagnostic consumer
+ */
+class BlankDiagConsumer : public clang::DiagnosticConsumer
+{
+  public:
+    BlankDiagConsumer() = default;
+    virtual ~BlankDiagConsumer() {}
+    virtual void HandleDiagnostic(DiagnosticsEngine::Level DiagLevel, const Diagnostic &Info) override {}
+};
+/**********************************************************************************************************************/
+class TBCFrontendAction : public ASTFrontendAction {
+public:
+  TBCFrontendAction() {}
+  ~TBCFrontendAction() {
+    delete BDC;
+  }
+  void EndSourceFileAction() override {
+    TheRewriter.getEditBuffer(TheRewriter.getSourceMgr().getMainFileID()).write(llvm::outs());
+  }
+
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef file) override {
+    DiagnosticsEngine &DE = CI.getPreprocessor().getDiagnostics();
+    DE.setClient(BDC, false);
+    TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
+    return llvm::make_unique<TBCASTConsumer>(TheRewriter);
+  }
+
+private:
+  BlankDiagConsumer *BDC = new BlankDiagConsumer;
+  Rewriter TheRewriter;
+};
+/**********************************************************************************************************************/
+/*Main*/
+int main(int argc, const char **argv) {
+  CommonOptionsParser op(argc, argv, TBCCategory);
+  ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+
+  return Tool.run(newFrontendActionFactory<TBCFrontendAction>().get());
+}
+/*last line intentionally left blank*/
+
