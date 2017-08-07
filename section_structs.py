5877d7b3da0250feb9d3f9f419078ebd7386bade
bloodstalker <thabogre@gmail.com>
2017-08-07 16:09:14 +0430

    changed the vars from class vars to instance vars

--- section_structs.py
+++ section_structs.py
@@ -1,36 +1,42 @@
 class Func_Type():
-    form = int()
-    param_cnt = int()
-    param_types = []
-    return_cnt = int()
-    return_type = []
+    def __init__(self):
+        self.form = int()
+        self.param_cnt = int()
+        self.param_types = []
+        self.return_cnt = int()
+        self.return_type = []
 
 
 class Global_Type():
-    content_type = int()
-    mutability = int()
+    def __init__(self):
+        self.content_type = int()
+        self.mutability = int()
 
 
 class Resizable_Limits():
-    flags = int()
-    initial = int()
-    maximum = int()
+    def __init__(self):
+        self.flags = int()
+        self.initial = int()
+        self.maximum = int()
 
 
 class Table_Type():
-    element_type = int()
-    limit = Resizable_Limits()
+    def __init__(self):
+        self.element_type = int()
+        self.limit = Resizable_Limits()
 
 
 class External_Kind():
-    Function = 0
-    Table = 1
-    Memory = 2
-    Global = 3
+    def __init__(self):
+        self.Function = 0
+        self.Table = 1
+        self.Memory = 2
+        self.Global = 3
 
 
 class Memory_Type():
-    limits = [Resizable_Limits()]
+    def __init__(self):
+        self.limits = [Resizable_Limits()]
 
 
 # @DEVI-FIXME-
@@ -39,118 +45,137 @@ class Init_Expr():
 
 
 class Type_Section():
-    count = []
-    func_types = []
+    def __init__(self):
+        self.count = []
+        self.func_types = []
 
 
 class Import_Entry():
-    module_len = int()
-    module_str = []
-    field_len = int()
-    field_str = []
-    kind = int()
-    type = int()
+    def __init__(self):
+        self.module_len = int()
+        self.module_str = []
+        self.field_len = int()
+        self.field_str = []
+        self.kind = int()
+        self.type = int()
 
 
 class Import_Section():
-    count = []
-    import_entry = []
+    def __init__(self):
+        self.count = []
+        self.import_entry = []
 
 
 class Function_Section():
-    count = []
-    type_section_index = [int()]
+    def __init__(self):
+        self.count = []
+        self.type_section_index = [int()]
 
 
 class Table_Section():
-    count = []
-    table_types = []
+    def __init__(self):
+        self.count = []
+        self.table_types = []
 
 
 class Memory_Section():
-    count = []
-    #Resizable_Limits
-    memory_types = []
+    def __init__(self):
+        self.count = []
+        # Resizable_Limits
+        self.memory_types = []
 
 
 class Global_Variable():
-    global_type = Global_Type()
-    init_expr = []
+    def __init__(self):
+        self.global_type = Global_Type()
+        self.init_expr = []
 
 
 class Global_Section():
-    count = []
-    # Global_Variable
-    global_variables = []
+    def __init__(self):
+        self.count = []
+        # Global_Variable
+        self.global_variables = []
 
 
 class Export_Entry():
-    field_len = int()
-    field_str = []
-    kind = int()
-    index = int()
+    def __init__(self):
+        self.field_len = int()
+        self.field_str = []
+        self.kind = int()
+        self.index = int()
 
 
 class Export_Section():
-    count = []
-    # Export_Entry
-    export_entries = []
+    def __init__(self):
+        self.count = []
+        # Export_Entry
+        self.export_entries = []
 
 
 class Start_Section():
-    function_section_index = []
+    def __init__(self):
+        self.function_section_index = []
 
 
 class Elem_Segment():
-    index = int()
-    offset = []
-    num_elem = int()
-    elems = []
+    def __init__(self):
+        self.index = int()
+        self.offset = []
+        self.num_elem = int()
+        self.elems = []
 
 
 class Element_Section():
-    count = []
-    # Elem_Segment
-    elem_segments = []
+    def __init__(self):
+        self.count = []
+        # Elem_Segment
+        self.elem_segments = []
 
 
 class Local_Entry():
-    count = int()
-    type = int()
+    def __init__(self):
+        self.count = int()
+        self.type = int()
 
 
 class WASM_Ins():
-    opcode = int()
-    operands = []
+    def __init__(self):
+        self.opcode = int()
+        self.operands = []
 
 
 class Func_Body():
-    body_size = int()
-    local_count = int()
-    # Local_Entry
-    locals = []
-    # WASM_Ins
-    code = []
-    end = int()
+    def __init__(self):
+        self.body_size = int()
+        self.local_count = int()
+        # Local_Entry
+        self.locals = []
+        # WASM_Ins
+        self.code = []
+        self.end = int()
 
 
 class Code_Section():
-    count = []
-    # Func_Body
-    func_bodies = []
+    def __init__(self):
+        self.count = []
+        # Func_Body
+        self.func_bodies = []
 
 
 class Data_Segment():
-    index = int()
-    offset = []
-    size = int()
-    data = []
+    def __init__(self):
+        self.index = int()
+        self.offset = []
+        self.size = int()
+        self.data = []
 
 
 class Data_Section():
-    count = []
-    # Data_Segment
-    data_segments = []
+    def __init__(self):
+        self.count = []
+        # Data_Segment
+        self.data_segments = []
 
 
 class Name_Type():
@@ -196,14 +221,14 @@ class Module():
     def __init__(self, type_section, import_section, function_section,
                  table_section, memory_section, global_section, export_section,
                  start_section, element_section, code_section, data_section):
-        self.type_section = Type_Section()
-        self.import_section = Import_Section()
-        self.function_section = Function_Section()
-        self.table_section = Table_Section()
-        self.memory_section = Memory_Section()
-        self.global_section = Global_Section()
-        self.export_section = Export_Section()
-        self.start_section = Start_Section()
-        self.element_section = Element_Section()
-        self.code_section = Code_Section()
-        self.data_section = Data_Section()
+        self.type_section = type_section
+        self.import_section = import_section
+        self.function_section = function_section
+        self.table_section = table_section
+        self.memory_section = memory_section
+        self.global_section = global_section
+        self.export_section = export_section
+        self.start_section = start_section
+        self.element_section = element_section
+        self.code_section = code_section
+        self.data_section = data_section
