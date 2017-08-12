eb747120a8e11f759cf49161f9ec8a8de9ea81c3
bloodstalker <thabogre@gmail.com>
2017-08-12 22:22:10 +0430

    holds the init expr interperter

---
+++ vm.py
@@ -0,0 +1,30 @@
+from OpCodes import WASM_OP_Code
+from utils import Colors
+from argparser import *
+
+
+# @DEVI-FIXME-MVP-only
+def init_interpret(expr):
+    offset = 0
+    byte, offset, dummy = Read(expr, offset, 'uint8')
+    const = int()
+
+    if byte == 65:
+        const, offset, dummy = Read(expr, offset, 'varuint32')
+    elif byte == 66:
+        const, offset, dummy = Read(expr, offset, 'varint64')
+    elif byte == 67:
+        const, offset, dummy = Read(expr, offset, 'uint32')
+    elif byte == 68:
+        const, offset, dummy = Read(expr, offset, 'uint64')
+    elif byte == 35:
+        pass
+    else:
+        raise Exception(Colors.red + "illegal opcode for an MVP init expr." + Colors.ENDC)
+
+    block_end, offset, dummy = Read(expr, offset, 'uint8')
+    if block_end != 11:
+        raise Exception(Colors.red + "init expr has no block end." + Colors.ENDC)
+
+    return(const)
+
