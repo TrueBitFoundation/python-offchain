006289467464e40af43f7a5de15ec5fd7eea0ac0
bloodstalker <thabogre@gmail.com>
2017-08-14 18:38:06 +0430

    the source file for merklization

---
+++ merklize.py
@@ -0,0 +1,73 @@
+import hashlib
+from math import log, floor
+
+
+def Flatten(machinestate):
+    flat_machinestate = []
+    temp_branch = []
+
+    if isinstance_of(machinestate, list):
+        for elem in machinestate:
+            if isinstance_of(elem, list):
+                temp_branch = elem
+                Flatten(temp_branch)
+            else:
+                flat_machinestate.append(elem)
+    else:
+        raise Exception("machinestate passed to Flatten is not a list instance")
+
+
+# @DEVI-expects a flattened machinestate
+class Merklizer():
+    def __init__ (self, machinestate):
+        self.machinestate = machinestate
+        height = log(len(machinestate), 2)
+
+        if height - floor(height) > 0.0:
+            self.height = int(height) + 1
+        else:
+            self.height = int(height)
+
+    def calcTreeLength():
+        length = int()
+        total_length = int()
+        length = len(self.machinestate)
+        while True:
+            if length == 2:
+                total_length += 1
+                break
+            if length % 2 == 0:
+                temp_length = length / 2
+            else:
+                temp_length = (length / 2) + 1
+
+            total_length += temp_length
+            length = temp_length
+
+        self.total_length = total_length
+
+    def allocateTree():
+        self.merkletree = ['\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' ] * self.calcTreeLength()
+
+    def merklize():
+        # create the first level hashes
+        for index in range(0, len(self.machinestate), 2):
+            current_leaf = self.machinestate[index]
+            current_hash = hashlib.sha256(current_leaf)
+            merkletree.append(current_hash.hexdigest())
+            try:
+                leaf_right = self.machinestate[index + 1]
+            except IndexError:
+                # current leaf is the last element so we there is no right leaf
+                leaf_right = int()
+            hash_right = hashlib.sha256(leaf_right)
+            merkletree.append(hash_right.hexdigest())
+
+        for level in range(0, self.height - 1):
+
+    def getTree():
+        return(self.total_length, self.merkletree)
+
+    def run():
+        self.calcTreeLength()
+        allocateTree()
