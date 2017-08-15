e4c64f9a9d042a2759cfb3b560cbfed47eacd87b
bloodstalker <thabogre@gmail.com>
2017-08-15 17:40:46 +0430

    changing the merklizer class. it still will take a flat input but the way we call it will be different

--- merklize.py
+++ merklize.py
@@ -1,7 +1,9 @@
 import hashlib
 from math import log, floor
+from copy import deepcopy
 
 
+# falttens the machinestate-unused
 def Flatten(machinestate):
     flat_ms = []
     for iter in machinestate.Linear_Memory:
@@ -11,6 +13,7 @@ def Flatten(machinestate):
     for iter in machinestate.Stack_Omni:
         flat_ms.append(iter)
 
+    '''
     for iter in machinestate.Index_Space_Function:
         flat_ms.append(iter)
 
@@ -22,13 +25,16 @@ def Flatten(machinestate):
 
     for iter in machinestate.Index_Space_Linear:
         flat_ms.append(iter)
+    '''
 
 
-
-# @DEVI-expects a flattened machinestate
+# expects to receive a flat list, creates a merkle tree for it.
 class Merklizer():
     def __init__ (self, machinestate):
         self.machinestate = machinestate
+        self.temp_level = machinestate
+        self.double_temp = []
+        self.treeindex = int()
         height = log(len(machinestate), 2)
 
         if height - floor(height) > 0.0:
@@ -36,46 +42,74 @@ class Merklizer():
         else:
             self.height = int(height)
 
-    def calcTreeLength():
-        length = int()
-        total_length = int()
+    def calcTreeLength(self):
         length = len(self.machinestate)
+        total_length = length
+        print('length: ', end='')
+        print(length)
         while True:
             if length == 2:
                 total_length += 1
                 break
             if length % 2 == 0:
-                temp_length = length / 2
+                temp_length = int(length / 2)
             else:
-                temp_length = (length / 2) + 1
+                temp_length = int(length / 2) + 1
 
             total_length += temp_length
             length = temp_length
 
         self.total_length = total_length
+        print(total_length)
+        return(total_length)
 
-    def allocateTree():
-        self.merkletree = ['\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' ] * self.calcTreeLength()
+    # we are allocating the tree as a flat list. the root hash will be the last
+    # element in the tree.
+    def allocateTree(self):
+        self.merkletree = ['\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'] * self.calcTreeLength()
 
-    def merklize():
-        # create the first level hashes
-        for index in range(0, len(self.machinestate), 2):
-            current_leaf = self.machinestate[index]
+    # create the first level hashes
+    def hashallleaves(self):
+        for leaf in self.machinestate:
+            current_hash = hashlib.sha256(leaf)
+            self.merkletree[self.treeindex] = current_hash.hexdigest()
+            self.treeindex += 1
+            self.double_temp.append(current_hash.hexdigest())
+
+        self.temp_level = deepcopy(self.double_temp)
+        self.double_temp = []
+
+    # creates the parent hashes
+    def merklize(self):
+        for index in range(0, len(self.temp_level), 2):
+            current_leaf = self.temp_level[index]
             current_hash = hashlib.sha256(current_leaf)
-            merkletree.append(current_hash.hexdigest())
-            try:
-                leaf_right = self.machinestate[index + 1]
-            except IndexError:
-                # current leaf is the last element so we there is no right leaf
-                leaf_right = int()
-            hash_right = hashlib.sha256(leaf_right)
-            merkletree.append(hash_right.hexdigest())
 
-        for level in range(0, self.height - 1):
+            if index + 1 > len(self.temp_level) - 1:
+                new_parent = hashlib.sha256(current_hash)
+                self.merkletree[self.treeindex] = new_parent.hexdigest()
+                self.treeindex += 1
+                self.double_temp.append(new_parent)
+            else:
+                leaf_right = self.temp_level[index + 1]
+                hash_right = hashlib.sha256(leaf_right)
+                new_parent = hashlib.sha256(current_hash + hash_right)
+                self.merkletree[self.treeindex] = new_parent.hexdigest()
+                self.treeindex += 1
+                self.double_temp.append(new_parent)
 
-    def getTree():
+            if len(self.temp_level) != 1:
+                self.temp_level = deepcopy(self.double_temp)
+                self.double_temp = []
+                self.merklize(self.temp_level)
+
+
+    # returns the tree length along with the tree itself
+    def getTree(self):
         return(self.total_length, self.merkletree)
 
-    def run():
-        self.calcTreeLength()
-        allocateTree()
+    def run(self):
+        self.allocateTree()
+        self.hashallleaves()
+        self.merklize()
+        return(self.getTree)
